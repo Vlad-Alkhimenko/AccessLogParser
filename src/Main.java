247beef6264b4f700b38488d3cd6db5ce4978617
Vladislav Alkhimenko <polyar999@mail.ru>
2025-08-10 17:45:12 +0800

    Добавил логику по подсчету строк в файле

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    private static File path;
+
+    public static void main(String[] args) throws CheсkLongLineException{
         // Выводим последовательно 2 числа в консоль
         System.out.println("Введите первое число:");
         int firstNumber=new Scanner(System.in).nextInt();
@@ -15,20 +19,19 @@ public class Main {
         System.out.println("Произведение: " + (firstNumber*secondNumber));
         System.out.println("Частное: " + ((double)firstNumber/secondNumber));
 
-        // Бесконечный цикл
-        // Создаем счетчик для хранения
-        int count=0;
+        // Цикл
+        int count=0; // Создаем счетчик для хранения
         System.out.println("Введите путь к файлу: ");
-        // Логика бесконечного цикла
+
         while (true) {
             String path=new Scanner(System.in).nextLine();
-            // Существует ли файл, путь к которому был указан
-            File file=new File(path);
-            // Переменная fileExists будет равна true, если файл существует, и false, если не существует
-            boolean fileExists=file.exists();
-            // Является ли указанный путь директорией
-            boolean isDirectory=file.isDirectory();
-            //
+
+            File file=new File(path); // Существует ли файл, путь к которому был указан
+
+            boolean fileExists=file.exists(); // Переменная fileExists будет равна true, если файл существует, и false, если не существует
+            boolean isDirectory=file.isDirectory(); // Является ли указанный путь директорией
+
+            // Проверка на наличие файла
             if (!fileExists || isDirectory) {
                 if (!fileExists) {
                     System.out.println("Указанный файл не существует.");
@@ -37,9 +40,56 @@ public class Main {
                 }
                 continue; // Продолжаем цикл
             }
-            count++;
             System.out.println("Путь указан верно. Это файл номер " + count);
+            count++;
             System.out.println("Общее количество верных путей: " + count);
+
+            // --- НАЧИНАЕМ ЧТЕНИЕ ФАЙЛА ---
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+
+                String line;
+                int totalLenght=0;
+                int minLenght=0;
+                int maxLenght=0;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+
+                    // Проверка на длину строки 1024
+                    if (length>1024) {
+                        throw new CheсkLongLineException("Строка длиннее 1024 символов!!!");
+                    }
+                    // Проверка на самую короткую строку
+                    if (length<minLenght) {
+                        maxLenght=length;
+                    }
+                    // Проверка на самую длинную строку
+                    if (length>maxLenght) {
+                        maxLenght=length;
+                    }
+
+                    totalLenght++;
+                }
+
+                System.out.println("Длина самой короткой строки в файле: "+minLenght);
+                System.out.println("Длина самой длинной строки в файле: "+maxLenght);
+                System.out.println("Общее количество строк в файле: "+totalLenght++);
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                System.out.println("Собрали статистику по файлу");
+            }
+        }
+    }
+
+    // Собственный класс исключения
+    static class CheсkLongLineException extends RuntimeException{
+        public CheсkLongLineException(String message) {
+            super(message);
         }
     }
 }
